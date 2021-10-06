402f93f1cb941acc2f3beee9755eb1a04bf7c0df
yessermiranda13 <yesser.miranda13@est.unanleon.edu.ni>
2021-10-06 14:49:30 -0600

    Mi primera prueba Junit 4 con una funcion de suma con Maven

---
+++ src/main/java/org/yesser/Aritmetica.java
@@ -0,0 +1,8 @@
+package org.yesser;
+
+public class Aritmetica {
+    public double sumar(double a, double b)
+    {
+        return a + b;
+    }
+}
---
+++ src/test/java/org/yesser/AritmeticaTest.java
@@ -0,0 +1,15 @@
+package org.yesser;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AritmeticaTest {
+    @Test
+    public void probarSuma()
+    {
+        Aritmetica arit = new Aritmetica();
+        Assert.assertEquals(10.0, arit.sumar(5.0, 5.0), 0.0001);
+        Assert.assertEquals(11.0, arit.sumar(6.0, 5.0), 0.0001);
+        Assert.assertEquals(10.0, arit.sumar(6.0, 4.0), 0.0001);
+    }
+}
